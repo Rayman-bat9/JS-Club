34d252d9af43dc3de8514d6e9bf3dc2ae5935491
YaroslavKravchenko <gaga63354@gmail.com>
2022-10-03 17:44:05 +0300

    homework (data-type-conversion) done;

---
+++ data-type-conversion/data-type-conversion-task2.js
@@ -0,0 +1,5 @@
+var x = 1;
+var y = 2;
+var res2 = Boolean(x) + '' + y ; // Допишіть код, необхідно використовувати змінні x та y
+console.log(res2); // "true2"
+console.log(typeof res2); // "string"
